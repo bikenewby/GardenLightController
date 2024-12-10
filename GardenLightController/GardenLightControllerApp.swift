6032043844b61a3f33422bd98ef49e015808cd86
KRIT SUDDUEN <krit@th.ibm.com>
2024-12-10 17:24:56 +0700

    Initial Commit

---
+++ GardenLightController/GardenLightControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenLightControllerApp.swift
+//  GardenLightController
+//
+//  Created by Krit Sudduen on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct GardenLightControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
